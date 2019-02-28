176cfa1803a915483ee1d7f1c2067444668b736c
Fachrul Kurniansyah <fchrulk@outlook.com>
2019-02-28 18:17:55 +0700

    Update chunks.py

--- chunks/chunks.py
+++ chunks/chunks.py
@@ -1,4 +1,16 @@
 def chunks(l, n):
+    """
+    Dividing list or dataframe intp n-sized chunks.
+    l = your_list
+    n = define size
+
+    Example:
+    my_list = ['a','b','c','d','e','f','g','h','i','j']
+    chunked_list = chunks(l=my_list, n=2)
+    print(chunked_list)
+    Output:
+    [['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h'], ['i', 'j']]
+    """
     new = []
     for i in range(0, len(l), n):    
         new.append(l[i:i + n])
