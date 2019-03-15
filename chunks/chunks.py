44ec0208fc1f725981f382113cd762e6913b28a4
Fachrul Kurniansyah <fchrulk@outlook.com>
2019-03-15 13:15:35 +0700

    Update chunks.py

--- chunks/chunks.py
+++ chunks/chunks.py
@@ -1,17 +1,29 @@
-def chunks(l, n):
+def chunks(l, n, N=None):
     """
     Dividing list or dataframe intp n-sized chunks.
     l = your_list
-    n = define size
+    N = define size
+    n = define size of each chunk
 
     Example:
     my_list = ['a','b','c','d','e','f','g','h','i','j']
     chunked_list = chunks(l=my_list, n=2)
     print(chunked_list)
+    
+    my_list = ['a','b','c','d','e','f','g','h','i','j']
+    chunked_list = chunks(l=my_list, N=2)
+    print(chunked_list)
+    
+    
     Output:
     [['a', 'b'], ['c', 'd'], ['e', 'f'], ['g', 'h'], ['i', 'j']]
+    [['a', 'b', 'c', 'd', 'e'], ['f', 'g', 'h', 'i', 'j']]
     """
     new = []
-    for i in range(0, len(l), n):    
-        new.append(l[i:i + n])
+    if (N is not None):
+        for i in range(0, len(l), N):    
+            new.append(l[i:i + N])
+    else:
+        for i in range(0, len(l), n):    
+            new.append(l[i:i + n])
     return new
